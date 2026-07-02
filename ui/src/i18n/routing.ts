6e8c14d6310085ba93ec960941ec1e094caa7097
andreab <andrea.batazzi@topcs.it>
2026-07-02 13:26:08 +0200

    fix: cookie-based i18n, remove [locale] routing to fix redirect loop

--- ui/src/i18n/routing.ts
+++ ui/src/i18n/routing.ts
@@ -4,7 +4,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["it", "en"],
   defaultLocale: "it",
-  localePrefix: "always",
+  localePrefix: "never",
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =

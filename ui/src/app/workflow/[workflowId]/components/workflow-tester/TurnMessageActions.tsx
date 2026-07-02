12f6b789b82fdc78afb18df13531e559566fb06f
andreab <andrea.batazzi@topcs.it>
2026-07-02 18:50:24 +0200

    i18n: fix TurnMessageActions const t declaration

--- ui/src/app/workflow/[workflowId]/components/workflow-tester/TurnMessageActions.tsx
+++ ui/src/app/workflow/[workflowId]/components/workflow-tester/TurnMessageActions.tsx
@@ -22,6 +22,7 @@ export function TurnMessageActions({
     onRewind,
     onEdit,
 }: TurnMessageActionsProps) {
+    const t = useTranslations("workflowList");
     return (
         <>
             <button
